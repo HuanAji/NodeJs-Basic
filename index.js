e83fdc57a3454a124049d392677c061cd151022b
HuanAji <123148327+HuanAji@users.noreply.github.com>
2026-04-11 00:15:14 +0700

    trying (put) update username with query

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 import express from "express";
 const app = express();
 const port = 3000;
+import bodyParser from "body-parser";
 // Routes / url / endpoint utama kita method GET
+
+app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
   response.send("utama");
 });
 
-app.get("/index", (req, res) => {
-  res.send("Hello World!");
+app.get("/hello", (req, res) => {
+  console.log({ urlParams: req.query });
+  res.send("Hello World! tralalala");
 });
 
 app.post("/login", (req, res) => {
-  console.log({ requestFromOutdside: req });
+  console.log({ requestFromOutdside: req.body });
   res.send("Login berhasil");
+});
+
+app.put("/username", (req, res) => {
+  console.log({ requestData: req.body });
+  res.send("Update berhasil");
 });
 
 // untuk runnig express dibalik layar
